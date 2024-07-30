571789b680bdd9714489caf28785aed22cbd9ba5
Christian Dalvit <chris.dalvit@gmail.com>
2024-07-30 11:52:01 +0200

    add global specifier to header

--- src/headers/convolution.h
+++ src/headers/convolution.h
@@ -5,6 +5,6 @@
 #include "convolution.h"
 
 void cpu_convolution(int image_dim_x, int image_dim_y, int* image, int K_dim, float* K, int* output);
-void gpu_convolution(int image_dim_x, int image_dim_y, int* image, int K_dim, float* K, int* output);
+__global__ void gpu_convolution(int image_dim_x, int image_dim_y, int* image, int K_dim, float* K, int* output);
 
 #endif
