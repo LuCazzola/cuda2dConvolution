3396a50e31f79db009774fce3b6b7f7e35a17a9a
chrisdalvit <chris.dalvit@gmail.com>
2024-08-08 10:38:56 +0200

    add padding handling in shared gpu convolution

--- src/convolution.cu
+++ src/convolution.cu
@@ -97,29 +97,66 @@ void gpu_convolution_shared(matrix image, matrix K, matrix output, const int W, 
 
     extern __shared__ matrix_element shared_image[];
 
-    for(int c = 0; c < C; c++){
-        shared_image[(threadIdx.x*blockDim.x)*C + threadIdx.y*C + c] = image[u*(W + 2*PAD)*C + v*C + c]; //Maybe remove padding
-    }
-
-   __syncthreads();
-
-    if(u >= PAD && v >= PAD && u < W+PAD && v < H+PAD){
-        int i,j;                // kernel indeces
-        int patch_u, patch_v;   // image pixel currently evaluated by kernel
-        int c;                  // channel index
-        const int K_CENTER = (int)(K_DIM / 2);
-        matrix_element sum;
-
-        for(c = 0; c < C; c++){
-            sum = 0.0;
-            for(i = 0; i < K_DIM; i++){
-                for(j = 0; j < K_DIM; j++){
-                        sum += K[i*K_DIM + j] * shared_image[(threadIdx.x - K_CENTER + i)*(blockDim.x + 2*PAD)*C + (threadIdx.y - K_CENTER + j)*C + c];
-                    }
-                }
-            output[(u-PAD)*W*C + (v-PAD)*C + c] = sum;
+    if(u < W+2*PAD && v < H+2*PAD){
+        for(int c = 0; c < C; c++){
+            shared_image[((threadIdx.x+PAD)*blockDim.x)*C + (threadIdx.y+PAD)*C + c] = image[u*(W + 2*PAD)*C + v*C + c]; //Maybe remove padding
         }
-    }
+        if(u >= PAD && v >= PAD && u < W+PAD && v < H+PAD){
+            // Thread of left edge must also copy left padding
+            if(threadIdx.x < PAD){
+                for(int c = 0; c < C; c++){
+                    shared_image[threadIdx.x*blockDim.x*C + (threadIdx.y+PAD)*C + c] = image[(u - PAD)*(W + 2*PAD)*C + v*C + c];
+                }
+            }
 
-    __syncthreads();
+            // Thread of top edge must also copy top padding
+            if(threadIdx.y < PAD){
+                for(int c = 0; c < C; c++){
+                    shared_image[(threadIdx.x+PAD)*blockDim.x*C + threadIdx.y*C + c] = image[u*(W + 2*PAD)*C +(v-PAD)*C + c];
+                }
+            }
+            // Thread of right edge must also copy right padding
+            if(threadIdx.x + PAD > blockDim.x){
+                for(int c = 0; c < C; c++){
+                    shared_image[(threadIdx.x + 2*PAD)*blockDim.x*C + (threadIdx.y+PAD)*C + c] = image[(u + PAD)*(W + 2*PAD)*C + v*C + c];
+                }
+            }
+            // Thread of bottom edge must also copy bottom padding
+            if(threadIdx.y + PAD > blockDim.y){
+                for(int c = 0; c < C; c++){
+                    shared_image[(threadIdx.x+PAD)*blockDim.x*C + (threadIdx.y + 2*PAD)*C + c] = image[u*(W + 2*PAD)*C +(v+PAD)*C + c];
+                }
+            }
+            // Thread for corner 
+
+            // Thread for corner 
+            
+            // Thread for corner 
+            
+            // Thread for corner 
+
+            __syncthreads();
+
+            int i,j;                // kernel indeces
+            int patch_u, patch_v;   // image pixel currently evaluated by kernel
+            int c;                  // channel index
+            const int K_CENTER = (int)(K_DIM / 2);
+            matrix_element sum;
+
+            for(c = 0; c < C; c++){
+                sum = 0.0;
+                for(i = 0; i < K_DIM; i++){
+                    for(j = 0; j < K_DIM; j++){
+                            patch_u = threadIdx.x + 2*PAD - K_CENTER + i;
+                            patch_v = threadIdx.y + 2*PAD - K_CENTER + j;
+
+                            sum += K[i*K_DIM + j] * shared_image[patch_u*C*blockDim.x + patch_v*C + c];
+                        }
+                    }
+                output[(u-PAD)*W*C + (v-PAD)*C + c] = sum;
+            }
+        }
+
+        __syncthreads();
+    }
 }
